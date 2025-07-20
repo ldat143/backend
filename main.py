22c4760439937ad8a4386d9f040fff6a9a20b933
ldat143 <ajhuacho.tarquino.luis.david@gmail.com>
2025-07-20 11:42:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from crews.competitor_crew import run_competitor_crew
 from crews.opportunity_crew import run_opportunity_crew
-from dotenv import load_dotenv
-load_dotenv()
+
 
 if __name__ == "__main__":
     inputs = {
         "zipcode": "83440",
         "dealership": "Liberty GMC",
         "customer": "Cox Automotive",
-        "person": "Luis",
-        "range" : "100"
+        "person": "Luis"
     }
 
     result1 = run_competitor_crew(**inputs)
